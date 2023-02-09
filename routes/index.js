67ef80918f8464e53a22cd966447c3202adebd63
eneko9555 <116375125+eneko9555@users.noreply.github.com>
2023-02-09 19:10:08 +0100

    commit

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import { paginaInicio, paginaNosotros, paginaOpiniones, paginaViajes, paginaDetalle } from '../controllers/paginasController.js';
+import { guardarOpinion } from '../controllers/opinionController.js';
+const router = express.Router();
+
+router.get('/', paginaInicio);
+
+router.get('/nosotros', paginaNosotros);
+
+router.get('/viajes', paginaViajes);
+router.get('/viajes/:slug', paginaDetalle);
+
+router.get('/opiniones', paginaOpiniones);
+router.post('/opiniones', guardarOpinion);
+
+
+export default router;
